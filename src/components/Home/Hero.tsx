ce7ebc104ce1d4f19f4c74f26dc6a8c0f1392a98
shihab-bhuiya <shihab.bhuiya07@gmail.com>
2026-09-15 11:56:17 +0600

    implementiong second row

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 import heroRightImg from "../../../public/hero/card-bg-of-cow.jpg";
 import heroLeftImg from "../../../public/hero/hero-left-img.jpg";
 import HerCow from "../../../public/hero/hero-right-cow.png";
+import LaunchImage from "../../../public/hero/near-to-launch.png";
+import handPhoneBg from "../../../public/hero/hand-phone-bg.png";
+import handPhone from "../../../public/hero/handPhone.png";
+import phone from "../../../public/hero/Home-Phone.png";
 
 const HeroSection = () => {
   return (
@@ -63,6 +67,52 @@ const HeroSection = () => {
 
 </div>
 </div>
+
+
+{/* second row */}
+
+
+    <div className="flex justify-between mt-4">
+      <div className="grid grid-cols-2 gap-6">
+
+  {/* Left column */}
+  <div className="flex flex-col gap-4">
+
+    {/* Stay Tuned */}
+    <div className="rounded-[24px] w-[300px] bg-[#C0612B] h-[258px]">
+
+     <h2 className="p-8 pt-14 font-bold  leading-none"> <span className="text-[#000000] text-[64px]">STAY </span> <br /> <span className="text-[64px] text-white">Tuned... </span>    </h2>    
+    </div>
+
+    {/* Launch */}
+    <div className="relative rounded-[24px] border border-[#C0612B] h-[130px]">
+   
+      <Image src={LaunchImage} alt="launch-image" fill className="absolute inset-0 object-fill rounded-lg" />
+    </div>
+
+  </div>
+
+  {/* Right column */}
+  <div className="relative rounded-[24px] min-h-[380px] overflow-hidden">
+    <Image src={handPhoneBg} alt="hand-phone-bg" fill className="object-fill rounded-lg" />
+
+      <Image src={handPhone} alt="hand-phone" width={247.2023468017578} height={446} className="absolute w-[247.2023468017578px] h-auto top-[42%] left-[46%] -translate-x-1/2 -translate-y-1/2" />
+
+      <Image src={phone} alt="phone" width={610} height={446} className="absolute w-auto h-auto top-[42%] left-[46%] -translate-x-1/2 -translate-y-1/2" />
+
+
+  </div>
+
+</div>
+
+
+{/* second row right side */}
+<div>
+  <h2>Second Row Content</h2>
+</div>
+
+
+    </div>
 
 
 
